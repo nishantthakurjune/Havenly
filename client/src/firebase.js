96cab46ccd63b221a3b44b2feeb5e9f6a39173c4
nishantthakurjune <nishantthakurjune@gmail.com>
2025-02-23 19:29:24 +0530

    google oauth added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "myestate-92d74.firebaseapp.com",
+  projectId: "myestate-92d74",
+  storageBucket: "myestate-92d74.firebasestorage.app",
+  messagingSenderId: "501937114015",
+  appId: "1:501937114015:web:8172a3a1b7ae886b82cbaa",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
